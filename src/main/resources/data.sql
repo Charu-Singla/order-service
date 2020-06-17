ee6c5aeda82b69e021096b735909b675c55dd97a
Charu-Singla <66715961+Charu-Singla@users.noreply.github.com>
2020-06-17 12:18:52 +0530

    micro service code

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into orders(order_id, user_id, order_amount, order_date)
+values (1, 1, 223.31, '14-Apr-2020');
+insert into orders(order_id, user_id, order_amount, order_date)
+values (2, 1, 3423, '15-Apr-2020');
+insert into orders(order_id, user_id, order_amount, order_date)
+values (3, 1, 623.31, '18-Apr-2020');
+insert into orders(order_id, user_id, order_amount, order_date)
+values (4, 2, 456, '12-Apr-2020');
+insert into orders(order_id, user_id, order_amount, order_date)
+values (5, 3, 343.31, '13-Apr-2020');
+insert into orders(order_id, user_id, order_amount, order_date)
+values (6, 3, 2342.31, '24-Apr-2020');
